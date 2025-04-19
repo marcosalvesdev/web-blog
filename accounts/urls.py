fb5d8e6b790fd50ce1e23f2adc061d64517b447c
marcosalves.dev <marcosalves.dev@gmail.com>
2025-04-19 19:56:26 -0300

    Project update structure

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,44 +1,47 @@
 from django.urls import path
-from accounts.views import (
-    UserLoginView,
-    UserLogoutView,
-    UserRegistrationView,
-    UserResetPasswordView,
-    UserResetDonePasswordView,
-    UserPasswordResetConfirmView,
-    UserPasswordResetCompleteView,
-    UserProfileView,
-)
+from accounts import views
 
 app_name = "accounts"
 
 urlpatterns = [
-    path("login/", UserLoginView.as_view(), name="login"),
-    path("logout/", UserLogoutView.as_view(), name="logout"),
-    path("register/", UserRegistrationView.as_view(), name="register"),
+    path("login/", views.UserLoginView.as_view(), name="login"),
+    path("logout/", views.UserLogoutView.as_view(), name="logout"),
+    path("register/", views.UserRegistrationView.as_view(), name="register"),
 ]
 
 # Profile URLs
 urlpatterns += [
-    path("profile/<int:pk>/", UserProfileView.as_view(), name="profile"),
+    path("profile/<str:slug>/", views.UserProfileView.as_view(), name="profile"),
+    path(
+        "profile/<str:slug>/update/",
+        views.UpdateProfileView.as_view(),
+        name="profile_update",
+    ),
+    path(
+        "profile/<str:slug>/update_image/",
+        views.UpdateProfileImageView.as_view(),
+        name="profile_update_image",
+    ),
 ]
 
 # Password Reset URLs
 urlpatterns += [
-    path("password/reset/", UserResetPasswordView.as_view(), name="password_reset"),
+    path(
+        "password/reset/", views.UserResetPasswordView.as_view(), name="password_reset"
+    ),
     path(
         "password/reset/done/",
-        UserResetDonePasswordView.as_view(),
+        views.UserResetDonePasswordView.as_view(),
         name="password_reset_done",
     ),
     path(
         "password/reset/<uidb64>/<token>/",
-        UserPasswordResetConfirmView.as_view(),
+        views.UserPasswordResetConfirmView.as_view(),
         name="password_reset_confirm",
     ),
     path(
         "password/reset/complete/",
-        UserPasswordResetCompleteView.as_view(),
+        views.UserPasswordResetCompleteView.as_view(),
         name="password_reset_complete",
     ),
 ]
--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("post/<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
     path("create/", views.CreatePostView.as_view(), name="create_post"),
-    path("about/", views.about_view, name="about"),
+    path("about/", views.AboutView.as_view(), name="about"),
 ]
